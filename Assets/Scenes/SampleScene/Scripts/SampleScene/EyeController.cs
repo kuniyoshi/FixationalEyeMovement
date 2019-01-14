7452b69a7c4afc472b93ccbdf3bfc694ff2cd12f
KUNIYOSHI Kouji <kuniyoshi.kouji@gmail.com>
2019-01-14 15:54:26 +0900

    Add SampleScene

---
+++ Assets/Scenes/SampleScene/Scripts/SampleScene/EyeController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace SampleScene
+{
+
+
+    public class EyeController : MonoBehaviour
+    {
+
+        const float DegreeSecondsToDegree = 60f / 60f;
+
+        public GameObject EyeIn;
+
+        public float Radius;
+
+        public float DegreeSeconds;
+
+        Vector3 _initialRotation;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Assert.IsNotNull(EyeIn);
+            _initialRotation = transform.rotation.eulerAngles;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = new Vector3(0f, 0f, Radius);
+            EyeIn.transform.localPosition = new Vector3(0f, 0f, -Radius);
+
+            var rotation = _initialRotation
+                           + new Vector3(0f, DegreeSeconds / DegreeSecondsToDegree, 0f);
+
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+
+    }
+
+}
