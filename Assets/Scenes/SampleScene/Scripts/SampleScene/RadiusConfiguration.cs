391dbac4d29d498865daafdc2ad019254d7f0efc
KUNIYOSHI Kouji <kuniyoshi.kouji@gmail.com>
2019-01-14 16:08:50 +0900

    Update SampleScene

--- Assets/Scenes/SampleScene/Scripts/SampleScene/RadiusConfiguration.cs
+++ Assets/Scenes/SampleScene/Scripts/SampleScene/RadiusConfiguration.cs
@@ -5,24 +5,17 @@ namespace SampleScene
 {
 
 
-    public class EyeController : MonoBehaviour
+    public class RadiusConfiguration : MonoBehaviour
     {
-
-        const float DegreeSecondsToDegree = 60f / 60f;
 
         public GameObject EyeIn;
 
         public float Radius;
 
-        public float DegreeSeconds;
-
-        Vector3 _initialRotation;
-
         // Start is called before the first frame update
         void Start()
         {
             Assert.IsNotNull(EyeIn);
-            _initialRotation = transform.rotation.eulerAngles;
         }
 
         // Update is called once per frame
@@ -30,11 +23,6 @@ namespace SampleScene
         {
             transform.position = new Vector3(0f, 0f, Radius);
             EyeIn.transform.localPosition = new Vector3(0f, 0f, -Radius);
-
-            var rotation = _initialRotation
-                           + new Vector3(0f, DegreeSeconds / DegreeSecondsToDegree, 0f);
-
-            transform.rotation = Quaternion.Euler(rotation);
         }
 
     }
